a8b350d05b1c8d30addb5f11865af0786b64c668
trung-code87 <minhtrung452@gmail.com>
2026-09-17 15:07:53 +0700

    Cap nhat du lieu qua cong cu web: TEST-DELETE-ME

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-const DASHBOARD_DATA_SOURCES = [];
+const DASHBOARD_DATA_SOURCES = [{"label":"TEST-DELETE-ME","generatedAt":"2026-09-17T08:07:53.022Z","datalo":[],"catBapMap":{},"dinhMucSections":[]}];
